c4a31693d972e798fe6fd0659863b0272ac2fa73
Zaro <matheus.freitas.cunha.19@gmail.com>
2024-08-28 12:27:15 -0300

    first commit

---
+++ src/PrimeiroProjeto/PrimeiroProjeto.java
@@ -0,0 +1,11 @@
+package PrimeiroProjeto;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		String nome = "Matheus"; 
+        System.out.println("Hello " + nome);
+
+	}
+
+}
